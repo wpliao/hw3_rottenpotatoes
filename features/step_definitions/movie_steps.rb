6a7e453b0c4e1a92834df1f67fd04d0e822ec7a5
wpliao <pei222@gmail.com>
2013-02-10 09:22:55 +0000

    Added all requirements in HW3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body =~ /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +25,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  if uncheck == "un"
+    rating_list.split(", ").each do |x|
+      step %{I uncheck "ratings_#{x}"}
+    end
+  else
+    rating_list.split(", ").each do |x|
+      step %{I check "ratings_#{x}"}
+    end
+  end
 end
+
+Then /I should see all of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == Movie.count
+end
+
